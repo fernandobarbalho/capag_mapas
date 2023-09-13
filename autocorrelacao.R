e49a2f7cfa3535c2a366e8f075734a7ac856a3a8
Fernando Almeida Barbalho <40247971+fernandobarbalho@users.noreply.github.com>
2023-09-13 16:02:20 -0300

    Mapas de clusters espaciais

--- autocorrelacao.R
+++ autocorrelacao.R
@@ -21,7 +21,7 @@ centroides<-
 
 
 # Access the shapefile
-s<-
+df_shape<-
 mapa_municipios[-2260,] %>%
   inner_join(
     dados_capag_2022 %>%
@@ -35,7 +35,7 @@ mapa_municipios[-2260,] %>%
 
 
 # select column to work with
-s <- subset(s, select=c("indicador_1"))
+s <- subset(df_shape, select=c("indicador_1"))
 
 s %>%
   slice_sample(prop=0.1) %>%
@@ -100,6 +100,54 @@ df_lisa<- as_tibble(lisa)
 
 names(df_lisa)
 
-lisa %>%
-  mutate(id= row_number()) %>%
-  filter()
+df_lisa %>%
+  mutate(id= as.character(row_number())) %>%
+  readr::write_csv("lisa.csv")
+
+
+df_lisa<- janitor::clean_names(df_lisa)
+
+
+df_lisa_significante<-
+  df_lisa %>%
+  mutate(id = row_number())%>%
+  filter(pr_z_e_ii <=0.05)
+
+
+df_vizinhos<-
+purrr::map_dfr(df_lisa_significante$id, function(a_id){
+
+
+  tibble(id=lw[["neighbours"]][[a_id]],
+         id_referencia= a_id,
+         ii= rep(df_lisa_significante$ii[which(df_lisa_significante$id== a_id)], NROW(lw[["neighbours"]][[a_id]]) ),
+         z_ii = rep(df_lisa_significante$z_ii[which(df_lisa_significante$id== a_id)], NROW(lw[["neighbours"]][[a_id]])))
+
+})
+
+
+df_clusters_espaciais<-
+  df_shape %>%
+  mutate(id = row_number())%>%
+  inner_join(
+    df_vizinhos %>%
+      group_by(id) %>%
+      filter(z_ii == max(z_ii)) %>%
+      ungroup()
+
+  )
+
+
+df_clusters_espaciais %>%
+  ggplot() +
+  geom_sf(aes(fill=ii))
+
+
+df_clusters_espaciais %>%
+  ggplot() +
+  geom_sf(aes(fill=z_ii))
+
+
+df_clusters_espaciais %>%
+  ggplot() +
+  geom_sf(aes(fill=indicador_1))
